cc4bf947034713e5cdb60f52fdc89fb3ae2397fd
brotato53 <themasterofgaming532@gmail.com>
2026-05-25 23:33:49 -0400

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ app.get('/', (req, res) => {
 </html>`);
 });
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0" () => {
   console.log(`Server running on port ${PORT}`);
 });
 
