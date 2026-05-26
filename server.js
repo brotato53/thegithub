c542dc2b95d316dea5dbdc4c6a791689e7b7d121
brotato53 <themasterofgaming532@gmail.com>
2026-05-25 22:04:59 -0400

    Remove UUID generation function
    Removed UUID generation function from server.js.

--- server.js
+++ server.js
@@ -590,12 +590,7 @@ wss.on('connection', (ws) => {
     }
   });
 });
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
+ 
 
 // Start the server
 const PORT = process.env.PORT || 3000;
