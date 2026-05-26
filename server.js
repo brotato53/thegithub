f4655153cdf12a747340f450b40506a6a62e3049
brotato53 <themasterofgaming532@gmail.com>
2026-05-25 22:02:41 -0400

    Remove uuidv4 function definition

--- server.js
+++ server.js
@@ -590,7 +590,6 @@ wss.on('connection', (ws) => {
     }
   });
 });
-function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
